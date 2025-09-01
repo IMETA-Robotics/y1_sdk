14d138fa0473eb95c62c99764ada2c1eef57201e
arron.zhu <arron.zhu@uditech.com.cn>
2025-09-01 14:03:29 +0800

    fix:update urdf path.

--- src/imeta_y1_controller/y1_controller.cpp
+++ src/imeta_y1_controller/y1_controller.cpp
@@ -43,14 +43,19 @@ bool Y1Controller::Init() {
   std::string urdf_path;
   if (arm_end_type == 0) {
     // only load robotic arm
-    urdf_path = package_path + "/urdf/y10824_ee.urdf";
+    urdf_path = package_path + "/urdf/y10804.urdf";
   } else if (arm_end_type == 1) {
-    // robotic arm and gripper
-    urdf_path = package_path + "/urdf/y10824_ee.urdf";
+    // robotic arm and gripper_T
+    urdf_path = package_path + "/urdf/y1_gripper_t.urdf";
   } else if (arm_end_type == 2) {
-    // robotic arm and teaching pendant
-    urdf_path = package_path + "/urdf/y1_arm_with_teaching_pendant.urdf";
-  } else {
+    // robotic arm and gripper_G
+    urdf_path = package_path + "/urdf/y1_gripper_g.urdf";
+  } else if(arm_end_type == 3){
+    // robotic arm and gripper_GT
+    // urdf_path = package_path + "/urdf/y1_gripper_gt.urdf";
+    urdf_path = package_path + "/urdf/y10824_ee.urdf";
+  }
+  else {
     ROS_ERROR("arm_end_type is %d , not supported", arm_end_type);
     return false;
   }
