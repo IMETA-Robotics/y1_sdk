eff0464b2648644fd81cbd2c7309b445317793ab
xiaofan Zhang <xiaofzhang1@gmail.com>
2025-09-15 00:58:32 +0800

    fix control scripts

--- scripts/control_scripts/joint_position_control.sh
+++ scripts/control_scripts/joint_position_control.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-rostopic pub /y1/arm_joint_position_control imeta_y1_msg/ArmJointPositionControl "header:
+rostopic pub /y1/arm_joint_position_control y1_msg/ArmJointPositionControl "header:
   seq: 0
-  stamp:
-    secs: 0
-    nsecs: 0
+  stamp: {secs: 0, nsecs: 0}
   frame_id: ''
-arm_joint_position: [0.6, -0.6, 0.6, 0.5, 0.4, 0]
-arm_joint_velocity: 0.5
-gripper: -20"
+joint_position: [0.6, -0.6, 0.6, 0.5, 0.4, 0]
+joint_velocity: 5
+gripper_stroke: 50 
+gripper_velocity: 5" 
